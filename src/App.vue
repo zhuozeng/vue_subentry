7d8b14c14c0657e46b573802fa465a1ad9833299
1124990187@qq.com <1124990187@qq.com>
2019-02-25 15:39:00 +0800

    引入vant-ui

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
+    <van-button type="default">默认按钮</van-button>
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
 
 <style>
 #app {
-  
 }
 </style>
