66acfe82590836573b5c337765fa37cea230d7bb
1124990187@qq.com <1124990187@qq.com>
2019-02-25 12:01:01 +0800

    完成min-ui引入

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+
+// 引入全部组件 
+import Mint from 'mint-ui';
+Vue.use(Mint);
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
